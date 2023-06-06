5653801bf4d1d89ee648bb6ce89f18b53ef865d6
AbtahiHasan <anikidpersonal@gmail.com>
2023-06-07 01:19:54 +0600

    verifyToken doen

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ const port = process.env.PORT || 3000
 const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+// verify jwt token 
+const verifyToken = (req, res, next) => {
+  const authorization = req.headers.authorization
+  if(!authorization) {
+     return res.status(401).send({error: true, message: "unauthorized access"})
+  }
 
+  const token = authorization.split(' ')[1]
+  jwt.verify(token, process.env.SECKRET_KEY, (error, decoded) => {
+      if(error) {
+         return res.status(401).send({error: true, message: "unauthorized access"})
+      }
+      req.decoded = decoded
+      next()
+  })
+}
 
 
 const uri = process.env.DB_URI;
